285ef8d0734d15f0ff3bbb4ebd962cc1d6e8643b
thcr <talmachlev3399@gmail.com>
2023-05-05 22:04:31 +0300

    Add ids tests and fixes

--- Algorithms/Ids.py
+++ Algorithms/Ids.py
@@ -5,36 +5,37 @@ from collections import deque
 
 from Util.utils import MoveTypes
 
+
 def get_new_boards(board: Board):
     possible_moves = []
     empty_item_pos = board.get_board_state().index(0)
     empty_row = empty_item_pos // board.width()
     empty_col = empty_item_pos % len(board[empty_row])
 
-    if empty_row + 1 < board.width() and board.can_move_up(empty_row + 1, empty_col):  # move empty item up
+    if board.can_move_right(empty_row, empty_col - 1):  # move empty item right
         new_board = Board(board.width(), board.get_board_state())
-        new_board.move_up(empty_row + 1, empty_col)
-        possible_moves.append((new_board, MoveTypes.UP))
-
-    if board.can_move_down(empty_row - 1, empty_col):  # move empty item down
-        new_board = Board(board.width(), board.get_board_state())
-        new_board.move_down(empty_row - 1, empty_col)
-        possible_moves.append((new_board, MoveTypes.DOWN))
+        new_board.move_right(empty_row, empty_col - 1)
+        possible_moves.append((new_board, MoveTypes.RIGHT))
 
     if empty_col + 1 < len(board[empty_row]) and board.can_move_left(empty_row, empty_col + 1):  # move empty item left
         new_board = Board(board.width(), board.get_board_state())
         new_board.move_left(empty_row, empty_col + 1)
         possible_moves.append((new_board, MoveTypes.LEFT))
 
-    if board.can_move_right(empty_row, empty_col - 1):  # move empty item right
+    if board.can_move_down(empty_row - 1, empty_col):  # move empty item down
         new_board = Board(board.width(), board.get_board_state())
-        new_board.move_right(empty_row, empty_col - 1)
-        possible_moves.append((new_board, MoveTypes.RIGHT))
+        new_board.move_down(empty_row - 1, empty_col)
+        possible_moves.append((new_board, MoveTypes.DOWN))
+
+    if empty_row + 1 < board.width() and board.can_move_up(empty_row + 1, empty_col):  # move empty item up
+        new_board = Board(board.width(), board.get_board_state())
+        new_board.move_up(empty_row + 1, empty_col)
+        possible_moves.append((new_board, MoveTypes.UP))
 
     return possible_moves
 
 
-def ids(board: Board):
+def Ids(board: Board, max_depth):
     layer = 0
 
     goal: list[int] = board.get_goal_state()
@@ -45,16 +46,18 @@ def ids(board: Board):
         stack = deque()
         stack.append(board)
         visited = set()
-        steps = run_dfs_in_layer(stack, visited, goal, layer)
+        steps = run_dfs_in_layer(stack, visited, goal, layer, [])
 
-        if steps is not None or len(visited) == len(goal):
+        if steps is not None or layer > max_depth:
             break
         layer += 1
 
-    return reversed(steps)
+    if not steps:
+        return []
 
+    return list(reversed(steps))
 
-def run_dfs_in_layer(stack: deque, visited, goal: list[int], layer: int):
+def run_dfs_in_layer(stack: deque, visited, goal: list[int], layer: int, array: list[MoveTypes]):
     node = stack.pop()
 
     if node.__str__() not in visited and layer > -1:
@@ -67,14 +70,14 @@ def run_dfs_in_layer(stack: deque, visited, goal: list[int], layer: int):
 
         moves = []
 
-        for new_board in new_boards:
-            stack.append(new_board[0])
-            steps_to_goal = run_dfs_in_layer(stack, visited, goal, layer - 1)
+        for new_board_inx in range(0, len(new_boards)):
+            stack.append(new_boards[new_board_inx][0])
+            steps_to_goal = run_dfs_in_layer(stack, visited, goal, layer - 1, array + [new_boards[new_board_inx][1]])
 
             if steps_to_goal is not None:
                 moves = steps_to_goal
 
-                moves.append(new_board[1])
+                moves.append(new_boards[new_board_inx][1])
 
         if moves:
             return moves
