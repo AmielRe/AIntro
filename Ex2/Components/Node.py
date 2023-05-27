8c9abfadabaa70a0b196a864bdbe669684f4d65e
AmielRe <amieli1999@gmail.com>
2023-05-27 18:18:48 +0300

    Create tree node component.

---
+++ Ex2/Components/Node.py
@@ -0,0 +1,7 @@
+class Node:
+    """Contains the information of the node and another nodes of the Decision Tree."""
+
+    def __init__(self):
+        self.value = None
+        self.next = None
+        self.childs = None
