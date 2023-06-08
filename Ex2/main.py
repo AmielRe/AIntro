c661d8301a4ccc536577da91ea30bc860d94dd92
AmielRe <amieli1999@gmail.com>
2023-06-08 20:43:42 +0300

    Organize main flow.
    Call methods in order needed.
    Print to both output_tree file and output file.
    Rename ID3 algorithm class.

--- Ex2/main.py
+++ Ex2/main.py
@@ -1,34 +1,30 @@
-from Ex2.Algorithms.NB import NaiveBayes
+from Algorithms.NB import NaiveBayes
 from Utils.file import File
 import pandas as pd
-from Algorithms.ID3 import DecisionTreeClassifier
+from Algorithms.ID3 import ID3
 
 def main():
     # Load training data from file
     data, feature_names, labels = File.load_train_data(r'Files/train.txt')
 
-    # Create ID3 classifier
-    tree_clf = DecisionTreeClassifier(data=data, feature_names=feature_names, labels=labels)
-
-    # Create the tree
+    # Create ID3 classifier and create the tree
+    tree_clf = ID3(data=data, feature_names=feature_names, labels=labels)
     tree_clf.id3()
+
+    # Create NaiveBayes
+    naive_base = NaiveBayes(data=data, feature_names=feature_names, labels=labels)
+    naive_base.nb()
+
+    # Load test data from file
+    test_data = pd.read_csv(r'Files/test.txt', sep="\t")
 
     # Write the tree to 'output_tree' file in required format
     with open(r'Files/output_tree.txt', 'w') as f:
         File.write_tree_output(f, tree_clf.node)
 
-    # Load test data from file
-    test_data = pd.read_csv(r'Files/test.txt', sep="\t")
-
-    # Evaluate and print the accuracy of the ID3
-    accuracy = tree_clf.evaluate(test_data, test_data.columns[-1])
-    print("Test accuracy {:.4f}".format(accuracy))
-
-    naive_base = NaiveBayes(data=data, feature_names=feature_names, labels=labels)
-
-    naive_base.nb()
-
-    print(naive_base.evaluate(test_data, test_data.columns[-1]))
+    # Write both algorithms result and accuracy
+    with open(r'Files/output.txt', 'w') as f:
+        File.write_output(f, tree_clf, naive_base, test_data, test_data.columns[-1])
 
 
 if __name__ == "__main__":
